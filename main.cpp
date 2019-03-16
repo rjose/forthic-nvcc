6b565ab5e6a07e846abde5e2f457078e4f90eb7b
Rino Jose <rino.jose@outlook.com>
2019-03-16 10:05:51 -0700

    Run a forthic file from the commandline

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <memory>
 #include <cstdio>
+#include <fstream>
+
 #include "Interpreter.h"
 #include "Module.h"
 #include "CudaModule.h"
 
 using namespace std;
 
-int main() {
+string load_file(string filename) {
+    ifstream infile { filename };
+    if (!infile) {
+        throw string("Can't open file: ") + filename;
+    }
+
+    string result { istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
+    return result;
+}
+
+int main(int c, char* argv[]) {
     try {
+        string filename = "CHECK-DIMENSION.forthic";
+        if (c >= 2)   filename = argv[1];
+
         Interpreter interp;
         interp.RegisterModule(shared_ptr<Module>(new CudaModule()));
-        interp.Run("[ cuda ] USE-MODULES");
-        interp.Run("1 15 HELLO");
+        interp.Run(load_file(filename));
     }
     catch (const char *message) {
         printf("EXCEPTION: %s\n", message);
